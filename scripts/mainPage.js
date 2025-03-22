6add62fc88e1fb11a868ac6cfd07b46d180dce4f
Tazo <klementizarkua5@gmail.com>
2025-03-22 19:13:58 +0400

    generate messages via javascript and style the inbox

---
+++ scripts/mainPage.js
@@ -0,0 +1,67 @@
+import { messages } from "./messages.js";
+
+
+let messageHTML = '';
+
+messages.forEach((message) => {
+messageHTML += `
+            <div class="message-line">
+                <div class="left-message-line">
+                    <input type="checkbox">
+                    <img src="sidebar-icons/starred.png">
+                    <p>${message.author}</p>
+                </div>
+                <div class="middle-message-line">
+                    <div class="message-theme">
+                        <P>${message.theme}</P>
+                    </div>
+                    <div class="message">
+                        <p id="message">${message.message}</p>
+                    </div>
+                </div>
+                <div class="right-message-line">
+                    <p>${message.time}</p>
+                </div>
+            </div>
+`;
+});
+
+document.querySelector('.chat-inbox').innerHTML = messageHTML;
+
+const selectForm = document.querySelector('.middle-form');
+
+selectForm.addEventListener('submit', (event) => {
+document.querySelector('.chat-inbox').innerHTML = '';
+event.preventDefault();
+const searchInput = document.getElementById('search-input');
+const key = searchInput.value.trim().toLowerCase();
+const searchedMessage = messages.filter(message => message.author.trim().toLowerCase().includes(key) || message.message.trim().toLowerCase().includes(key));
+let searchedHTML = '';
+if(searchedMessage){
+    searchedMessage.forEach((message) => {
+searchedHTML += `
+
+            <div class="message-line">
+                <div class="left-message-line">
+                    <input type="checkbox">
+                    <img src="sidebar-icons/starred.png">
+                    <p>${message.author}</p>
+                </div>
+                <div class="middle-message-line">
+                    <div class="message-theme">
+                        <P>${message.theme}</P>
+                    </div>
+                    <div class="message">
+                        <p id="message">${message.message}</p>
+                    </div>
+                </div>
+                <div class="right-message-line">
+                    <p>${message.time}</p>
+                </div>
+            </div>
+
+`
+    })
+}
+document.querySelector('.chat-inbox').innerHTML = searchedHTML;
+} )
---
+++ scripts/messages.js
@@ -0,0 +1,98 @@
+export const messages = [
+    {
+        author: 'Reddit',
+        theme: 'New message from u/welcomebot',
+        message: 'Welcome to r/Doppleganger! Before you get started, please be sure to read the rules of the community. Keep in mind that we do not allow anything NSFW as we have users ages 13+. NSFW users may use a se...',
+        time: '3:02AM'
+    },
+    {
+        author: 'The Postman Team',
+        theme: '250088 is your account verification code',
+        message: 'Please use the code to complete your verification and start collaborating in Postman.250088 This code will expire in 10 minutes. Alternatively, you can use the link below to verify your account.',
+        time: 'Mar 20'
+    },
+    {
+        author: 'Instagram',
+        theme: "Your Information Couldn't Be Confirmed",
+        message: "We weren't able to confirm your identity from the video you submitted. You can submit a new video and we'll review it again. Learn more.Thanks,The Instagram Team",
+        time: 'Mar 20'    
+    },
+    {
+        author: 'Instagram',
+        theme: "We received your request",
+        message: "Thanks for submitting your selfie video. We received this information and it's pending review. Your request may take a few business days.Thanks,The Instagram Team",
+        time: 'Mar 20'    
+    },
+    {
+        author: 'GitHub',
+        theme: "[GitHub] A first-party GitHub OAuth application has been added to your account",
+        message: "Hey TazoZarkua!A first-party GitHub OAuth application (Git Credential Manager) with gist, repo, and workflow scopes was recently authorized to access your account.Visit https://github.com/settings/connections/applications/0120e057bd645470c1ed for more information.To see this and other security events for your account, visit https://github.com/settings/security-logIf you run into problems, please contact support by visiting https://github.com/contactThanks,The GitHub Team",
+        time: 'Mar 20'    
+    },
+    {
+        author: 'JetBrains Team',
+        theme: "How was your experience with PyCharm so far?",
+        message: "Hi,Thank you for evaluating PyCharm!We would be thrilled to hear about your experience. Please leave your feedback and help us improve PyCharm in the future. It will take less than 3 minutes.",
+        time: 'Mar 20'    
+    },
+    {
+        author: 'Reddit',
+        theme: "Welcome to Reddit!",
+        message: "Good karma starts hereKarma is what redditors jokingly refer to as “fake internet points”, but it reflects the real contributions people make to their communities. When you upvote someone’s posts and comments, they get positive karma for their good contributions. And when your posts and comments get upvoted, you’ll start to earn karma too.",
+        time: 'Mar 20'    
+    },
+    {
+        author: 'Google',
+        theme: "Klementi, finish setting up your realme C53 device with Google",
+        message: "Hi Klementi,A better Android experienceis waitingTake one minute to set up your realme C53 with Google",
+        time: 'Mar 13'    
+    },
+    {
+        author: 'Google',
+        theme: "Klementi, finish setting up your realme C53 device with Google",
+        message: "Hi Klementi,A better Android experienceis waitingTake one minute to set up your realme C53 with Google",
+        time: 'Mar 13'    
+    },
+    {
+        author: 'JetBrains Team',
+        theme: "PyCharm Pro Subscription Options",
+        message: "Continuity Discount All licenses qualify for a 20% discount when renewed for a second year, and for a 40% discount when renewed from the third consecutive year onwards.",
+        time: 'Mar 6'    
+    },
+    {
+        author: 'X',
+        theme: "New login to X from ChromeDesktop on Windows",
+        message: "We noticed a login to your account @Molodoybosssss from a new device. Was this you?",
+        time: 'Feb 27'    
+    },
+    {
+        author: 'Spotify',
+        theme: "Confirm your account",
+        message: "WELCOME TO SPOTIFY",
+        time: 'Feb 3'    
+    },
+    {
+        author: 'WeTransfer',
+        theme: "blockchain web.zip sent successfully to lukiazerozero1999@gmail.com",
+        message: "blockchain web.zip sent to lukiazerozero1999@gmail.com",
+        time: 'Feb 3'    
+    },
+    {
+        author: 'YouTube Premium',
+        theme: "Congrats – you’re now a YouTube Premium member",
+        message: "Welcome to YouTube Premium",
+        time: 'Jan 27'    
+    },
+    {
+        author: 'Figma',
+        theme: "Congrats on joining your team in Figma!",
+        message: "We're so glad you're here. Figma connects everyone in the design process so teams can deliver better products, faster.Here are a few resources to help you get the most out of Figma:",
+        time: 'Jan 25'    
+    },
+    {
+        author: 'Samsung account',
+        theme: "მოგესალმებათ Samsung სერვისები.",
+        message: "მოგესალმებათ Samsung სერვისები.ძვირფასო მომხმარებელო,თქვენი ანგარიში ახლა გააქტიურებულია. ახლა შეგიძლიათ ისარგებლოთ სხვადასხვა Samsung სერვისზე წვდომით!",
+        time: 'Jan 12'    
+    },
+]
