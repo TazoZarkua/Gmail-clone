ffbd0a1d9a967b622466db6dca1954390742086c
Tazo <klementizarkua5@gmail.com>
2025-03-23 17:58:05 +0400

    manipulate the dom

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -138,3 +138,83 @@ icon.addEventListener('click', () => {
     }
 })
 
+const section_select_1 = document.querySelector('.section-select-1');
+const section_select_2 = document.querySelector('.section-select-2');
+const section_select_3 = document.querySelector('.section-select-3');
+const section_select_4 = document.querySelector('.section-select-4');
+let scr1 = document.getElementById('scr1');
+let scr1_blue = document.getElementById('scr1-blue');
+let scr2 = document.getElementById('scr2');
+let scr2_blue = document.getElementById('scr2-blue');
+let scr3 = document.getElementById('scr3');
+let scr3_blue = document.getElementById('scr3_blue');
+let src4 = document.getElementById('src4');
+let src4_blue = document.getElementById('src4_blue')
+section_select_1.addEventListener('click', () => {
+    if(section_select_2.classList.contains('section-select-inbox') || section_select_3.classList.contains('section-select-inbox') || section_select_4.classList.contains('section-select-inbox')){
+        section_select_2.classList.remove('section-select-inbox');
+        section_select_3.classList.remove('section-select-inbox');
+        section_select_4.classList.remove('section-select-inbox');
+    }
+    section_select_1.classList.add('section-select-inbox');
+    scr3.style.display = 'flex';
+    scr3_blue.style.display = 'none'
+    scr2.style.display = 'flex';
+    scr2_blue.style.display = 'none';
+    scr1.style.display = 'flex';
+    scr1_blue.style.display = 'none';
+    src4.style.display = 'flex';
+    src4_blue.style.display = 'none';
+});
+
+section_select_2.addEventListener('click', () => {
+    if(section_select_1.classList.contains('section-select-inbox') || section_select_3.classList.contains('section-select-inbox') || section_select_4.classList.contains('section-select-inbox')){
+        section_select_1.classList.remove('section-select-inbox');
+        section_select_3.classList.remove('section-select-inbox');
+        section_select_4.classList.remove('section-select-inbox');
+    }
+    section_select_2.classList.add('section-select-inbox');
+
+    scr2.style.display = 'none';
+    scr2_blue.style.display = 'flex';
+    scr1.style.display = 'none';
+    scr1_blue.style.display = 'flex';
+    scr3.style.display = 'flex';
+    scr3_blue.style.display = 'none';
+    src4.style.display = 'flex';
+    src4_blue.style.display = 'none';
+});
+
+section_select_3.addEventListener('click', () => {
+    if(section_select_1.classList.contains('section-select-inbox') || section_select_2.classList.contains('section-select-inbox') || section_select_4.classList.contains('section-select-inbox')){
+        section_select_1.classList.remove('section-select-inbox');
+        section_select_2.classList.remove('section-select-inbox');
+        section_select_4.classList.remove('section-select-inbox');
+    }
+    section_select_3.classList.add('section-select-inbox');
+    scr2.style.display = 'flex';
+    scr2_blue.style.display = 'none';
+    scr1.style.display = 'none';
+    scr1_blue.style.display = 'flex';
+    scr3.style.display = 'none';
+    scr3_blue.style.display = 'flex';
+        src4.style.display = 'flex';
+    src4_blue.style.display = 'none';
+});
+
+section_select_4.addEventListener('click', () => {
+    if(section_select_1.classList.contains('section-select-inbox') || section_select_2.classList.contains('section-select-inbox') || section_select_3.classList.contains('section-select-inbox')){
+        section_select_1.classList.remove('section-select-inbox');
+        section_select_2.classList.remove('section-select-inbox');
+        section_select_3.classList.remove('section-select-inbox');
+    }
+    section_select_4.classList.add('section-select-inbox');
+    scr2.style.display = 'flex';
+    scr2_blue.style.display = 'none';
+    scr1.style.display = 'none';
+    scr1_blue.style.display = 'flex';
+    scr3.style.display = 'flex';
+    scr3_blue.style.display = 'none';
+    src4.style.display = 'none';
+    src4_blue.style.display = 'flex'
+});
