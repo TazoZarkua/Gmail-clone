73351c0c00373654e88f865c1e9c2f3d39463787
Tazo <klementizarkua5@gmail.com>
2025-03-23 21:08:29 +0400

    customizing search input

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -6,7 +6,7 @@ import { updates } from "./updates.js";
 let messageHTML = '';
 
 messages.forEach((message) => {
-messageHTML += `
+    messageHTML += `
             <div class="message-line">
                 <div class="left-message-line">
                     <input type="checkbox">
@@ -31,17 +31,16 @@ messageHTML += `
 document.querySelector('.chat-inbox').innerHTML = messageHTML;
 
 const selectForm = document.querySelector('.middle-form');
-
 selectForm.addEventListener('submit', (event) => {
-document.querySelector('.chat-inbox').innerHTML = '';
-event.preventDefault();
-const searchInput = document.getElementById('search-input');
-const key = searchInput.value.trim().toLowerCase();
-const searchedMessage = messages.filter(message => message.author.trim().toLowerCase().includes(key) || message.message.trim().toLowerCase().includes(key));
-let searchedHTML = '';
-if(searchedMessage){
-    searchedMessage.forEach((message) => {
-searchedHTML += `
+    document.querySelector('.chat-inbox').innerHTML = '';
+    event.preventDefault();
+    const searchInput = document.getElementById('search-input');
+    const key = searchInput.value.trim().toLowerCase();
+    const searchedMessage = messages.filter(message => message.author.trim().toLowerCase().includes(key) || message.message.trim().toLowerCase().includes(key));
+    let searchedHTML = '';
+    if (searchedMessage) {
+        searchedMessage.forEach((message) => {
+            searchedHTML += `
 
             <div class="message-line">
                 <div class="left-message-line">
@@ -63,10 +62,10 @@ searchedHTML += `
             </div>
 
 `
-    })
-}
-document.querySelector('.chat-inbox').innerHTML = searchedHTML;
-} );
+        })
+    }
+    document.querySelector('.chat-inbox').innerHTML = searchedHTML;
+});
 
 document.querySelector('#message-count').innerHTML = messages.length;
 
@@ -75,67 +74,67 @@ document.querySelector('#message-count').innerHTML = messages.length;
 const checkbox = document.getElementById('js-checkbox');
 
 checkbox.addEventListener('click', () => {
-   let messagebox = document.querySelector('.message-status');
-       if(messagebox.style.display === 'flex'){
-            messagebox.style.display = 'none'
-       }
-       else{
+    let messagebox = document.querySelector('.message-status');
+    if (messagebox.style.display === 'flex') {
+        messagebox.style.display = 'none'
+    }
+    else {
         messagebox.style.display = 'flex';
-       }
- 
+    }
+
 });
 
 document.getElementById('checkbox').addEventListener('click', () => {
-   const header_container = document.querySelector('.header-container');
-   const header_container_2 = document.querySelector('.header-container-2');
-   const left_side_backup = document.querySelector('.left-side-backup');
-   const line = document.querySelectorAll('.message-line')
-if(header_container.style.display === 'none' && header_container_2.style.display === 'none' && left_side_backup.style.display === 'flex'){
-    header_container.style.display = 'flex';
-    header_container_2.style.display = 'flex'
-    left_side_backup.style.display = 'none';
-    line.forEach((lin) => {
-        lin.style.backgroundColor = 'white'
-    });
+    const header_container = document.querySelector('.header-container');
+    const header_container_2 = document.querySelector('.header-container-2');
+    const left_side_backup = document.querySelector('.left-side-backup');
+    const line = document.querySelectorAll('.message-line')
+    if (header_container.style.display === 'none' && header_container_2.style.display === 'none' && left_side_backup.style.display === 'flex') {
+        header_container.style.display = 'flex';
+        header_container_2.style.display = 'flex'
+        left_side_backup.style.display = 'none';
+        line.forEach((lin) => {
+            lin.style.backgroundColor = 'white'
+        });
 
-}
-else{
-    header_container.style.display = 'none';
-    header_container_2.style.display = 'none'
-    left_side_backup.style.display = 'flex';
-    line.forEach((lin) => {
-        lin.style.backgroundColor = 'rgb(195, 219, 255)'
-    });
-   let check = document.querySelector('#checkbox');
-   let IndividualCheckbox = document.querySelectorAll('#IndividualCheckbox')   
-}
+    }
+    else {
+        header_container.style.display = 'none';
+        header_container_2.style.display = 'none'
+        left_side_backup.style.display = 'flex';
+        line.forEach((lin) => {
+            lin.style.backgroundColor = 'rgb(195, 219, 255)'
+        });
+        let check = document.querySelector('#checkbox');
+        let IndividualCheckbox = document.querySelectorAll('#IndividualCheckbox')
+    }
 });
 
 
 const ExpandMore = document.getElementById('expand-more');
 ExpandMore.addEventListener('click', () => {
-   const sidebar =  document.querySelector('.on-show-icons');
-   const arrow_down = document.getElementById('arrow-down');
-   const arrow_up = document.getElementById('arrow-up')
-   if(sidebar.style.display === 'flex'){
-    sidebar.style.display = 'none';
-    arrow_up.style.display = 'none';
-    arrow_down.style.display = 'flex';
-   }
-   else{
-    sidebar.style.display = 'flex';
-    arrow_up.style.display = 'flex';
-    arrow_down.style.display = 'none'
-   }
+    const sidebar = document.querySelector('.on-show-icons');
+    const arrow_down = document.getElementById('arrow-down');
+    const arrow_up = document.getElementById('arrow-up')
+    if (sidebar.style.display === 'flex') {
+        sidebar.style.display = 'none';
+        arrow_up.style.display = 'none';
+        arrow_down.style.display = 'flex';
+    }
+    else {
+        sidebar.style.display = 'flex';
+        arrow_up.style.display = 'flex';
+        arrow_down.style.display = 'none'
+    }
 })
 
 const icon = document.getElementById('showBaseline');
 icon.addEventListener('click', () => {
     const baseline = document.querySelector('.baseline-icons');
-    if(baseline.style.display === 'flex'){
+    if (baseline.style.display === 'flex') {
         baseline.style.display = 'none'
     }
-    else{
+    else {
         baseline.style.display = 'flex'
     }
 })
@@ -153,7 +152,7 @@ let scr3_blue = document.getElementById('scr3_blue');
 let src4 = document.getElementById('src4');
 let src4_blue = document.getElementById('src4_blue')
 section_select_1.addEventListener('click', () => {
-    if(section_select_2.classList.contains('section-select-inbox') || section_select_3.classList.contains('section-select-inbox') || section_select_4.classList.contains('section-select-inbox')){
+    if (section_select_2.classList.contains('section-select-inbox') || section_select_3.classList.contains('section-select-inbox') || section_select_4.classList.contains('section-select-inbox')) {
         section_select_2.classList.remove('section-select-inbox');
         section_select_3.classList.remove('section-select-inbox');
         section_select_4.classList.remove('section-select-inbox');
@@ -168,12 +167,46 @@ section_select_1.addEventListener('click', () => {
     src4.style.display = 'flex';
     src4_blue.style.display = 'none';
     document.querySelector('.chat-inbox').innerHTML = messageHTML;
+    selectForm.addEventListener('submit', (event) => {
+        document.querySelector('.chat-inbox').innerHTML = '';
+        event.preventDefault();
+        let searchHTML = '';
+        event.preventDefault();
+        const searchInput = document.getElementById('search-input');
+        const key = searchInput.value.trim().toLowerCase();
+        let filtered = messages.filter(message => message.author.trim().toLowerCase().includes(key) || message.message.trim().toLowerCase().includes(key));
+        if (filtered) {
+            filtered.forEach((message) => {
+                searchHTML += `
+                        <div class="message-line">
+                <div class="left-message-line">
+                    <input type="checkbox" id="IndividualCheckbox">
+                    <img src="sidebar-icons/starred.png">
+                    <p>${message.author}</p>
+                </div>
+                <div class="middle-message-line">
+                    <div class="message-theme">
+                        <P>${message.theme}</P>
+                    </div>
+                    <div class="message">
+                        <p id="message">${message.message}</p>
+                    </div>
+                </div>
+                <div class="right-message-line">
+                    <p>${message.time}</p>
+                </div>
+            </div>
+            `
+            })
+            document.querySelector('.chat-inbox').innerHTML = searchHTML;
+        }
+    })
 });
 
 section_select_2.addEventListener('click', () => {
     document.querySelector('.chat-inbox').innerHTML = '';
     let promotionsHTML = '';
-    if(section_select_1.classList.contains('section-select-inbox') || section_select_3.classList.contains('section-select-inbox') || section_select_4.classList.contains('section-select-inbox')){
+    if (section_select_1.classList.contains('section-select-inbox') || section_select_3.classList.contains('section-select-inbox') || section_select_4.classList.contains('section-select-inbox')) {
         section_select_1.classList.remove('section-select-inbox');
         section_select_3.classList.remove('section-select-inbox');
         section_select_4.classList.remove('section-select-inbox');
@@ -211,13 +244,45 @@ section_select_2.addEventListener('click', () => {
         `
     });
     document.querySelector('.chat-inbox').innerHTML = promotionsHTML;
-
+    selectForm.addEventListener('submit', (event) => {
+        document.querySelector('.chat-inbox').innerHTML = '';
+        let searchHTML = '';
+        event.preventDefault();
+        const searchInput = document.getElementById('search-input');
+        const key = searchInput.value.trim().toLowerCase();
+        const searchedMessage = promoMessage.filter(message => message.author.trim().toLowerCase().includes(key) || message.message.trim().toLowerCase().includes(key));
+        if (searchedMessage) {
+            searchedMessage.forEach((message) => {
+                searchHTML += `
+                            <div class="message-line">
+                <div class="left-message-line">
+                    <input type="checkbox" id="IndividualCheckbox">
+                    <img src="sidebar-icons/starred.png">
+                    <p>${message.author}</p>
+                </div>
+                <div class="middle-message-line">
+                    <div class="message-theme">
+                        <P>${message.theme}</P>
+                    </div>
+                    <div class="message">
+                        <p id="message">${message.message}</p>
+                    </div>
+                </div>
+                <div class="right-message-line">
+                    <p>${message.time}</p>
+                </div>
+            </div>
+        `
+            })
+            document.querySelector('.chat-inbox').innerHTML = searchHTML;
+        }
+    })
 });
 
 section_select_3.addEventListener('click', () => {
     document.querySelector('.chat-inbox').innerHTML = '';
     let socialHTML = '';
-    if(section_select_1.classList.contains('section-select-inbox') || section_select_2.classList.contains('section-select-inbox') || section_select_4.classList.contains('section-select-inbox')){
+    if (section_select_1.classList.contains('section-select-inbox') || section_select_2.classList.contains('section-select-inbox') || section_select_4.classList.contains('section-select-inbox')) {
         section_select_1.classList.remove('section-select-inbox');
         section_select_2.classList.remove('section-select-inbox');
         section_select_4.classList.remove('section-select-inbox');
@@ -229,10 +294,10 @@ section_select_3.addEventListener('click', () => {
     scr1_blue.style.display = 'flex';
     scr3.style.display = 'none';
     scr3_blue.style.display = 'flex';
-        src4.style.display = 'flex';
+    src4.style.display = 'flex';
     src4_blue.style.display = 'none';
     socialMessages.forEach((message) => {
-   socialHTML += `
+        socialHTML += `
                      <div class="message-line">
                 <div class="left-message-line">
                     <input type="checkbox" id="IndividualCheckbox">
@@ -254,12 +319,45 @@ section_select_3.addEventListener('click', () => {
    `
     })
     document.querySelector('.chat-inbox').innerHTML = socialHTML;
+    selectForm.addEventListener('submit', (event) => {
+        document.querySelector('.chat-inbox').innerHTML = '';
+        event.preventDefault();
+        let filteredHTML = '';
+        const searchInput = document.getElementById('search-input');
+        const key = searchInput.value.trim().toLowerCase();
+        let filtered = messages.filter(message => message.author.trim().toLowerCase().includes(key) || message.message.trim().toLowerCase().includes(key));
+        if (filtered) {
+            filtered.forEach((message) => {
+                filteredHTML += `
+        <div class="message-line">
+            <div class="left-message-line">
+                <input type="checkbox" id="IndividualCheckbox">
+                <img src="sidebar-icons/starred.png">
+                <p>${message.author}</p>
+            </div>
+            <div class="middle-message-line">
+                <div class="message-theme">
+                    <P>${message.theme}</P>
+                </div>
+                <div class="message">
+                    <p id="message">${message.message}</p>
+                </div>
+            </div>
+            <div class="right-message-line">
+                <p>${message.time}</p>
+            </div>
+        </div>
+                 `
+            })
+            document.querySelector('.chat-inbox').innerHTML = filteredHTML;
+        }
+    })
 });
 
 section_select_4.addEventListener('click', () => {
     document.querySelector('.chat-inbox').innerHTML = '';
     let updateHTML = '';
-    if(section_select_1.classList.contains('section-select-inbox') || section_select_2.classList.contains('section-select-inbox') || section_select_3.classList.contains('section-select-inbox')){
+    if (section_select_1.classList.contains('section-select-inbox') || section_select_2.classList.contains('section-select-inbox') || section_select_3.classList.contains('section-select-inbox')) {
         section_select_1.classList.remove('section-select-inbox');
         section_select_2.classList.remove('section-select-inbox');
         section_select_3.classList.remove('section-select-inbox');
@@ -274,7 +372,7 @@ section_select_4.addEventListener('click', () => {
     src4.style.display = 'none';
     src4_blue.style.display = 'flex';
     updates.forEach((message) => {
-     updateHTML += `
+        updateHTML += `
                           <div class="message-line">
                 <div class="left-message-line">
                     <input type="checkbox" id="IndividualCheckbox">
@@ -296,4 +394,37 @@ section_select_4.addEventListener('click', () => {
      `
     });
     document.querySelector('.chat-inbox').innerHTML = updateHTML;
+    selectForm.addEventListener('submit', (event) => {
+        document.querySelector('.chat-inbox').innerHTML = '';
+        event.preventDefault();
+        let filteredHTML = '';
+        const searchInput = document.getElementById('search-input');
+        const key = searchInput.value.trim().toLowerCase();
+        let filtered = messages.filter(message => message.author.trim().toLowerCase().includes(key) || message.message.trim().toLowerCase().includes(key));
+        if(filtered){
+            filtered.forEach((message) => {
+                filteredHTML += `
+                                          <div class="message-line">
+                <div class="left-message-line">
+                    <input type="checkbox" id="IndividualCheckbox">
+                    <img src="sidebar-icons/starred.png">
+                    <p>${message.author}</p>
+                </div>
+                <div class="middle-message-line">
+                    <div class="message-theme">
+                        <P>${message.theme}</P>
+                    </div>
+                    <div class="message">
+                        <p id="message">${message.message}</p>
+                    </div>
+                </div>
+                <div class="right-message-line">
+                    <p>${message.time}</p>
+                </div>
+            </div>
+                `
+            })
+            document.querySelector('.chat-inbox').innerHTML = filteredHTML;
+        }
+    })
 });
